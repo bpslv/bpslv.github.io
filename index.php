db47b5c4fce4475be71de2461df6369b10377853
bpslv <bpslv@mac.com>
2020-10-02 13:48:34 -0700

    Initial upload
    Initial upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body style="background-color:#000;">
+
+<div style="position:relative; height:550px; width:960px; margin:0 auto; background-image:url(images/index_bkgrd.png); background-repeat:no-repeat;">
+  <div style="width:350px; padding-top:10px; margin:0 auto; position: relative; top: 310px; border-top:1px solid #676767; text-align: center; font-family: Helvetica, Arial, sans-serif; font-size: 15px; color: #676767">p: 702.296.8783&nbsp;&nbsp;e: brian@bpstephens.com</div>
+</div>
+</body>
+</html>
